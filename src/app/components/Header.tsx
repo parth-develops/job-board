07a468b6b4452d17328cc07eef12b3c370f4a894
Parth Umrania <pumrania9@gmail.com>
2024-10-09 12:41:01 +0530

    Added workos auth

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import {
+    getSignInUrl,
+    withAuth
+} from '@workos-inc/authkit-nextjs';
 
-export default function Header() {
+export default async function Header() {
+    const { user } = await withAuth();
+    const signInUrl = await getSignInUrl()
+
     return (
         <header className="">
             <div className="container flex items-center justify-between mx-auto my-4">
                 <Link href={'/'} className="font-bold text-xl">Job Board </Link>
                 <nav className="flex gap-4 *:py-2 *:px-4 *:rounded-md" >
-                    <Link className="bg-gray-200" href={"/login"}>Login</Link>
+                    {!user && <Link className="bg-gray-200" href={signInUrl}>Login</Link>}
                     <Link className="bg-blue-600 text-white" href={"/new-listing"}>Post a Job</Link>
                 </nav>
             </div>
