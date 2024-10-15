0f8f33fcc73e1ff5be913fd3e0d55f75fa01398c
Parth Umrania <pumrania9@gmail.com>
2024-10-15 17:15:03 +0530

    Developed create company page

---
+++ src/app/new-listing/[orgId]/page.tsx
@@ -0,0 +1,36 @@
+import { withAuth } from "@workos-inc/authkit-nextjs"
+import { WorkOS } from "@workos-inc/node";
+
+type PageProps = {
+    params: { orgId: string }
+}
+
+export default async function OrganizationPage(props: PageProps) {
+    const { user } = await withAuth();
+    const workos = new WorkOS(process.env.WORKOS_API_KEY);
+
+    if (!user) {
+        return <div className="container">Please log in to continue</div>
+    }
+
+    const orgId = props.params.orgId;
+
+    const orgMemberships = await workos.userManagement.listOrganizationMemberships({ userId: user.id, organizationId: orgId })
+    const hasAccess = orgMemberships.data.length > 0;
+
+    if (!hasAccess) {
+        return "no access";
+    }
+
+    const orgMembership = orgMemberships.data.find(om => orgId === om.organizationId)
+
+    return (
+        <div className='container'>
+            <form action="" className="">
+                <input type="text"
+                    className="border p-2"
+                />
+            </form>
+        </div>
+    )
+}
